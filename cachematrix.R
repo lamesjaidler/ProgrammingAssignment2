434b1cf5cc5a4154f0c113736798d4bdc9429835
lamesjaidler <james@simility.com>
2018-04-16 15:22:13 +0100

    Update

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to create a special object that stores an invertible matrix and caches it's inverse
 
-## Write a short comment describing this function
+## Creates a special "matrix", which is really a list containing a function to:
+## 1) Set the value of the matrix
+## 2) Get the value of the matrix
+## 3) Set the value of the inverse
+## 4) Get the value of the inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -20,7 +23,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Calculates the inverse of the special "matrix" created in the above function. 
+## Checks to see if inverse has already been calculated - if so, gets the inverse from
+## cache, otherwise calculates the inverse and sets the value of the inverse in the cache
+## via the setsolve function
 
 cacheSolve <- function(x, ...) {
 
